1084fb0d5483a4b01e5b51caf5bff9ca5de903d7
M Hazwan <37287035+hazxone@users.noreply.github.com>
2020-06-17 15:43:51 +0800

    Change size to 256

--- yolov4/yololayer.h
+++ yolov4/yololayer.h
@@ -15,8 +15,8 @@ namespace Yolo
     static constexpr float IGNORE_THRESH = 0.1f;
     static constexpr int MAX_OUTPUT_BBOX_COUNT = 1000;
     static constexpr int CLASS_NUM = 80;
-    static constexpr int INPUT_H = 352;
-    static constexpr int INPUT_W = 352;
+    static constexpr int INPUT_H = 256;
+    static constexpr int INPUT_W = 256;
 
     struct YoloKernel
     {
